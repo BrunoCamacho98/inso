78123bc1b70e2059de895fc68dfecc8ec67886b4
DarleySaavedra <31896714+DarleySaavedra@users.noreply.github.com>
2017-11-03 16:09:47 -0500

    Sprint1

---
+++ inso/Controllers/MateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inso.Controllers
+{
+    public class MateController : Controller
+    {
+        // GET: Mate
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
